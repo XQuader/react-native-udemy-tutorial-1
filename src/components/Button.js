f6c4ffd08376a1104c5dc9a75147f4463f4cf9f9
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-16 14:55:05 +0300

    Open browser on button click

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = (props) => {
+const Button = ({ onPress, children }) => {
     const { buttonStyle, textStyle } = styles;
 
     return (
-        <TouchableOpacity style={buttonStyle} onPress={props.onPress}>
+        <TouchableOpacity style={buttonStyle} onPress={onPress}>
             <Text style={textStyle}>
-                {props.children}
+                {children}
             </Text>
         </TouchableOpacity>
     );
