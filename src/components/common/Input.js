c00e2980d110259c321bd140f3bf860970d6d56e
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-17 13:10:46 +0300

    Add last common components

---
+++ src/components/common/Input.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = (props) => (
+      <View style={styles.containerStyle}>
+          <Text style={styles.labelStyle}>{props.label}</Text>
+          <TextInput
+              autoCorrect={false}
+              secureTextEntry={props.secureTextEntry}
+              placeholder={props.placeholder}
+              style={styles.inputStyle}
+              onChangeText={props.onChangeText}
+              value={props.value}
+          />
+      </View>
+);
+
+
+const styles = {
+    containerStyle: {
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1
+    },
+    inputStyle: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    }
+};
+
+export { Input };
