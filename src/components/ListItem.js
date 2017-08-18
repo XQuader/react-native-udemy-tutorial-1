f414d08d174797470823c3f75d8d939f5e36d8f1
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-18 16:11:23 +0300

    add selected item state and simple animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,34 +1,62 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback, LayoutAnimation, UIManager } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
+import { selectLibrary } from '../actions';
 
 class ListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
+    renderDecription() {
+        const { library, isSelected } = this.props;
+
+        if (isSelected) {
+            return (
+                <CardSection>
+                    <Text>{library.description}</Text>
+                </CardSection>
+            );
+        }
+    }
+
     render() {
-        const { id, title, description } = this.props.library;
+        const { id, title } = this.props.library;
         const { titleStyle } = styles;
 
         return (
-            <CardSection key={id}>
-                <Text style={titleStyle}>{title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection key={id}>
+                        <Text style={titleStyle}>{title}</Text>
+                    </CardSection>
+                    {this.renderDecription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
 
 const styles = {
+    containerStyle: {
+
+    },
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
     },
     descriptionStyle: {
-
-    },
-    activeStyle: {
-
+        padding: 10
     }
 };
 
-const mapStateToProps = ({ selectedLibraryId }) => ({ selectedLibraryId });
+const mapStateToProps = (state, ownProps) => ({ isSelected: state.selectedLibraryId === ownProps.library.id });
 
-export default connect(mapStateToProps)(ListItem);
+export default connect(mapStateToProps, { selectLibrary })(ListItem);
