0718084a29e58d115e09ab573c3b37ab117e39bc
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-18 17:08:08 +0300

    Add firebase connection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header, Card, CardSection, Button } from './components/common';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+    state = {
+        authenticated: null
+    };
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCPL868sIN8BY_nlSiZj6YSmIf_f-9knI8',
+            authDomain: 'manager-92f46.firebaseapp.com',
+            databaseURL: 'https://manager-92f46.firebaseio.com',
+            projectId: 'manager-92f46',
+            storageBucket: 'manager-92f46.appspot.com',
+            messagingSenderId: '1096667969153'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            this.setState({ authenticated: !!user });
+        });
+    }
+
     render() {
         return (
             <View>
-                <Header>Tech Stack</Header>
-                <Card>
-                    <CardSection>
-                        <Button>Click me</Button>
-                    </CardSection>
-                    <CardSection>
-                        <Button>Stop me</Button>
-                    </CardSection>
-                </Card>
+                <Header>Manager</Header>
             </View>
         );
     }
