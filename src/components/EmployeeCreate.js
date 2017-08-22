07c59601b4438e4baec7131b61c434bbd0382c0c
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-22 18:43:24 +0300

    Add inputs to EmployeeCreate

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Create</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Jane"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="555-555-5555"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Button>Create</Button>
+                </CardSection>
+            </Card>
         );
     }
 }
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 
 class EmployeeList extends Component {
