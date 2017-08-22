12b7e23aae484d11681e653f018453dea8cf831e
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-08-22 16:38:09 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, CardSection, Spinner, Button } from './components/common';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
-    state = {
-        authenticated: null
-    };
-
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyCPL868sIN8BY_nlSiZj6YSmIf_f-9knI8',
@@ -18,37 +12,11 @@ class App extends Component {
             storageBucket: 'manager-92f46.appspot.com',
             messagingSenderId: '1096667969153'
         });
-
-        firebase.auth().onAuthStateChanged((user) => {
-            this.setState({ authenticated: !!user });
-        });
-    }
-
-    renderContent() {
-        switch (this.state.authenticated) {
-            case true:
-                return (
-                    <CardSection>
-                        <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
-                    </CardSection>
-                );
-            case false:
-                return <LoginForm />;
-            default:
-                return (
-                    <CardSection>
-                        <Spinner />
-                    </CardSection>
-                );
-        }
     }
 
     render() {
         return (
-            <View>
-                <Header>Manager</Header>
-                {this.renderContent()}
-            </View>
+            <Router />
         );
     }
 }
